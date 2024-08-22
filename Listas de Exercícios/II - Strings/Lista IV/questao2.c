58e1e34dd1dc63d4ca8ab7481b43547451e1e8a6
DavyAndrade <hallszindementahhh@gmail.com>
2024-08-22 19:14:56 -0300

    Lista IV Completa

--- Listas de Exercícios/II - Strings/Lista IV/questao2.c
+++ Listas de Exercícios/II - Strings/Lista IV/questao2.c
@@ -8,3 +8,62 @@ sequência, independente se s é percorrida da
 esquerda para direita ou vice-versa.
 */
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int palindromo(char string[]);
+
+int main()
+{
+    char text[50];
+
+    printf("Digite o Texto: ");
+    fgets(text, sizeof(text), stdin);
+
+    text[strcspn(text, "\n")] = '\0';
+
+    if (palindromo(text))
+    {
+        printf("A string eh um palindromo.\n");
+    }
+    else
+    {
+        printf("A string nao eh um palindromo.\n");
+    }
+}
+
+int palindromo(char string[])
+{
+    int i;
+    int j = strlen(string) - 1;
+    int tam = strlen(string);
+
+    for (int i = 0; i < tam; i++)
+    {
+        if (string[i] == '\n')
+        {
+            string[i] = '\0';
+            break;
+        }
+    }
+
+    for (i = 0; string[i]; i++)
+    {
+        string[i] = tolower(string[i]);
+    }
+
+    i = 0;
+
+    while (i < j)
+    {
+        if (string[i] != string[j])
+        {
+            return 0;
+        }
+
+        i++, j--;
+    }
+
+    return 1;
+}
